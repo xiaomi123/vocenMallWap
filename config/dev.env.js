60cc77f0188e6f9b52f58e2420c245a918820d61
游客min <809610328@qq.com>
2020-11-11 13:45:36 +0800

    ac

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,13 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-<<<<<<< HEAD
-  //BASE_URL:'"http://10.0.3.188:8002/api/"'
-=======
-  //BASE_URL:'"http://10.0.3.188:8002/api/"',
-  //BASE_URL:'"http://localhost:56912/api/"',
->>>>>>> 24e75fb573496eccf7c7124013d93a82a1990b0b
+  BASE_URL:'"http://10.0.3.188:8002/api/"'
   //BASE_URL: '"http://10.0.2.233:56912/api/"'
-  BASE_URL:'"http://120.76.144.84:8088/api/"'
+  //BASE_URL:'"http://120.76.144.84:8088/api/"'
 
 })
